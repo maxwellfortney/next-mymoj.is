9c93a73a7f6bffdd3546b6b46dbdf2982ea35460
Maxwell Fortney <maxwellfortney@gmail.com>
2021-04-08 13:10:40 -0400

    test for claims path

--- pages/ipfs-404.tsx
+++ pages/ipfs-404.tsx
@@ -2,10 +2,23 @@ import Link from "next/link";
 import { useEffect } from "react";
 
 export default function Redirect() {
+    function loadPage(href: string) {
+        var xmlhttp = new XMLHttpRequest();
+        xmlhttp.open("GET", href, false);
+        xmlhttp.send();
+        return xmlhttp.responseText;
+    }
+
     useEffect(() => {
         console.log(window.history);
         console.log(window.location);
         console.log(document.referrer);
+
+        // window.history.pushState({});
+        if (window.location.href.includes("claim")) {
+            document.documentElement.innerHTML = loadPage("/claim.html");
+            window.history.replaceState(null, "", "/claim");
+        }
     }, []);
     return <div className="">REDIRECT</div>;
 }
