e6df46382cc9cc7e702fe648256d520ea9aafa0a
Maxwell Fortney <maxwellfortney@gmail.com>
2021-04-07 22:48:59 -0400

    Link test

---
+++ pages/claim.js
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function Claim() {
+    return (
+        <div className="">
+            claim
+            <Link href="/">HOME LINK</Link>
+        </div>
+    );
+}
